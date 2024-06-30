317833a2bcd46091856fec061932d54298056682
flutter-abhi <flutter.abhishek@gmail.com>
2024-06-30 16:23:52 +0530

    added code in main

---
+++ src/main/java/com/petguardian/views/VetarnaryView.java
@@ -0,0 +1,22 @@
+package com.petguardian.views;
+
+import com.petguardian.controllers.Pet;
+
+import javafx.scene.layout.Pane;
+
+public class VetarnaryView {
+    private Pet app;
+    private Pane rootpane;
+    boolean islogin = true;
+
+    public VetarnaryView(Pet app) {
+        this.app = app;
+        initialize();
+    }
+
+    private void initialize() {
+
+        
+    }
+
+}
