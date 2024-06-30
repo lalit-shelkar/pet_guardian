c7c0a60fad50700ad1834c2ac27316dd29853952
flutter-abhi <flutter.abhishek@gmail.com>
2024-06-30 14:11:19 +0530

    firebase intilize

--- src/main/java/com/petguardian/Main.java
+++ src/main/java/com/petguardian/Main.java
@@ -1,12 +1,34 @@
 package com.petguardian;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.petguardian.controllers.Pet;
 
 import javafx.application.Application;
 
-
 public class Main {
+    static {
+        try {
+            FileInputStream serviceAccount = new FileInputStream(
+                    "petgaurdian\\src\\main\\resources\\fir-javafx-47969-firebase-adminsdk-44ci7-18f1e5b923.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://javafx-project1-83368-default-rtdb.asia-southeast1.firebasedatabase.app")
+                    .build();
+            FirebaseApp.initializeApp(options);
+
+        } catch (IOException e) {
+            System.out.println("error");
+            System.err.println(e);
+        }
+    }
+
     public static void main(String[] args) {
-        Application.launch(Pet.class,"Hello world!");
+        Application.launch(Pet.class, "Hello world!");
     }
 }
