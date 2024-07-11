78dfc7e3f8620c2a6c83458680889dfb6c40e75a
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-07-12 03:04:07 +0530

    w

--- src/main/java/com/petguardian/firebase/InitilazationFirebase.java
+++ src/main/java/com/petguardian/firebase/InitilazationFirebase.java
@@ -11,11 +11,7 @@ public class InitilazationFirebase {
 
         public static void initialize() throws IOException {
                 FileInputStream serviceAccount = new FileInputStream(
-<<<<<<< HEAD
                                 "C:\\Users\\Dell\\Desktop\\JAVA-PRAC\\JavaFX\\petgaurdian\\src\\main\\resources\\fir-javafx-47969-firebase-adminsdk-44ci7-d6afe1530a.json");
-=======
-                                "C:\\Users\\abhis\\Desktop\\JAVAFX\\project\\pet_guardian\\src\\main\\resources\\fir-javafx-47969-firebase-adminsdk-44ci7-d6afe1530a.json");
->>>>>>> 8bbe36aca9be104e171571fec6fca2e36ec07050
 
                 FirebaseOptions options = new FirebaseOptions.Builder()
                                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
